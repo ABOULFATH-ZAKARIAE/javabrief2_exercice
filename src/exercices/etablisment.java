280d1c5f071fc265af21569add051beceaecfd92
ABOULFATH-ZAKARIAE <aboulfath.zakariae@gmail.com>
2020-12-04 16:59:25 +0100

    exercice

---
+++ src/exercices/etablisment.java
@@ -0,0 +1,20 @@
+package exercices;
+
+public class etablisment {
+	private String nom;
+	private int nbetudiant;
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getNbetudiant() {
+		return nbetudiant;
+	}
+	public void setNbetudiant(int nbetudiant) {
+		this.nbetudiant = nbetudiant;
+	}
+	
+}
